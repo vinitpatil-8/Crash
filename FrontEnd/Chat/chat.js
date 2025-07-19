43ee90d39a7cb65a1dec9c8c636de27af1b04397
Vinit <vinit.2007.20@gmail.com>
2025-07-19 21:48:10 +0530

    Initial Commit

---
+++ FrontEnd/Chat/chat.js
@@ -0,0 +1,149 @@
+let menuBtn = document.querySelector('.menuBtn'); // 3 lines button
+let closeBtn = document.querySelector('.closeBtn'); // Cross button
+let sideBar = document.querySelector('.sidebar'); // Sidebar
+let main = document.querySelector('.main'); // main chat screen
+let chatBox = document.querySelector('.chatBox') // the chat box
+let textArea = document.querySelector('#txtBox'); // Typing area
+let microphone = document.querySelector('.micBtn'); // Microphone
+let typingBox = document.querySelector('.typingBox'); // The whole typing area
+let sidebarBtns = document.querySelector('.sidebarBtns'); // Sidebar buttons area
+let winWidth = window.innerWidth; // Get the width of the window
+
+
+menuBtn.addEventListener('click', () => {
+    let windowWidth = window.innerWidth; // Get the width of the window
+    if (windowWidth<=768 && windowWidth>430){
+        sideBar.style.width = "19%"; // Expand sidebar
+        main.style.width = "81%";
+    }else if(windowWidth<=430){
+        sideBar.style.width = "30%"; // Expand sidebar
+        main.style.width = "70%";
+        textArea.disabled = "true" // disabling typing area
+        main.style.opacity = "0.5" // making typing area and chat area opaque
+        microphone.classList.toggle("no") // disabling microphone hover
+    }else{
+        sideBar.style.width = "15%"; // Expand sidebar
+        main.style.width = "85%";
+    }
+    menuBtn.style.display = "none"; // Hide the 3 lines button
+    setTimeout(() => {
+        sidebarBtns.style.display = "flex"; // Show the buttons after a interval
+        closeBtn.style.display = "block"; // Show the cross button after a interval
+    }, 900);
+});
+
+closeBtn.addEventListener('click', () => {
+    let windowWidth = window.innerWidth; // Get the width of the window
+    if (windowWidth<=768 && windowWidth>430){
+        sideBar.style.width = "8%"; // Collapse sidebar
+        main.style.width = "92%";
+    }else if(windowWidth<=430){
+        sideBar.style.width = "10%"; // Collapse sidebar
+        main.style.width = "90%";
+        textArea.disabled = false // enabling typing area
+        main.style.opacity = "1" // making typing area and chat as it was
+        microphone.classList.toggle("no") //enabling microphone hover
+    }else{
+        sideBar.style.width = "4%"; // Collapse sidebar
+        main.style.width = "96%";
+    }
+    menuBtn.style.display = "block"; // Show the 3 lines button
+    closeBtn.style.display = "none"; // Hide the cross button
+    sidebarBtns.style.display = "none"; // Hide the buttons
+    
+});
+
+// adding the contents of typing area to chat area
+textArea.addEventListener('keypress', function(event) {
+    if(event.key === 'Enter') {
+        event.preventDefault();
+        let val = textArea.value.trim();
+        if (val !== '') {
+            displayMessage(val);
+            textArea.value = '';
+        }
+    }
+});
+
+// function to display message
+function displayMessage(text) {
+  const msgSpan = document.createElement('span');
+  msgSpan.textContent = text;
+  chatBox.appendChild(msgSpan);
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+//scroll function
+main.addEventListener('wheel', function(e){
+    e.preventDefault();
+    const isOverTypingBox = textArea.matches(':hover');
+    const isOverType = typingBox.matches(':hover');
+    if (isOverTypingBox) {
+        textArea.scrollTop += e.deltaY;
+    } else {
+        if(isOverType){
+        // do nothing
+        }else{
+            chatBox.scrollTop += e.deltaY;
+        }
+    }
+}, { passive: false })
+
+// touch support for scroll function
+
+let startY = 0;
+let activeScrollTarget = null;
+
+main.addEventListener('touchstart', (e) => {
+    startY = e.touches[0].clientY;
+
+    const touch = e.touches[0];
+    const touchedElement = document.elementFromPoint(touch.clientX, touch.clientY);
+
+    if (textArea.contains(touchedElement)) {
+      activeScrollTarget = textArea;
+    } else {
+        if (typingBox.contains(touchedElement)) {
+            // do nothing
+        }else{
+            activeScrollTarget = chatBox;
+        }
+      
+    }
+});
+
+main.addEventListener('touchmove', (e) => {
+    if (!activeScrollTarget) return;
+
+    const currentY = e.touches[0].clientY;
+    const deltaY = startY - currentY;
+    startY = currentY;
+
+    activeScrollTarget.scrollTop += deltaY;
+    e.preventDefault(); // block default page scroll
+  }, { passive: false });
+
+main.addEventListener('touchend', () => {
+    activeScrollTarget = null;
+});
+
+
+
+// toggle dark mode and light mode
+
+const toggle = document.getElementById('themeToggle');
+
+toggle.addEventListener('change', () => {
+  document.body.classList.toggle('dark');
+  microphone.classList.toggle('fi-ss-circle-microphone');
+  microphone.classList.toggle('fi-ts-circle-microphone');
+});
+
+// checks if the user's OS has dark mode as preference
+window.addEventListener('DOMContentLoaded', () => {
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  if (prefersDark) {
+    document.body.classList.add('dark');
+    toggle.checked = true;
+  }
+});
